388b0f3bcb7a7a360885c14d5237baeee4d5b008
ampesce19 <39268723+ampesce19@users.noreply.github.com>
2018-06-15 12:03:47 -0400

    Calculating winning score and adding descriptions

--- app/quiz.py
+++ app/quiz.py
@@ -4,7 +4,7 @@
 
 import random
 
-#a=sandwich (reliable, always there), b=ice cream sundae (sweet, crowd-pleasing), c=hot dog (quirky, not for everyone), d= pizza (big personality, life of the party)  
+#a=sandwich (reliable, always there), b=ice cream sundae (sweet, crowd-pleasing), c=sushi (quirky, not for everyone), d= pizza (big personality, life of the party)  
 
 questions=["What is your dream job?","Who is your ideal BFF?","What animal are you most like?","What city do you want to move to?","What is your ideal Friday night?","What is your favorite thing to drink?","How often do you try new foods?","Where do you like to shop?"]
 a=["Accountant","Jason Bateman","Cat","Cleveland","Movie night with friends","Water","Never","Department store"]
@@ -16,3 +16,39 @@ atotal=0
 btotal=0
 ctotal=0
 dtotal=0
+
+#To-do: prompt users for each question and record results 
+
+#Calculate winner
+best_score = 0
+winner = ""
+if atotal > best_score:
+  best_score = atotal
+  winner = "a"
+if btotal > best_score:
+  best_score = btotal
+  winner = "b"
+if ctotal > best_score:
+  best_score = ctotal
+  winner = "c"
+if dtotal > best_score:
+  best_score = dtotal
+  winner = "d"
+winning_result = ""
+winning_paragraph = ""
+if winner == "a":
+  winning_result = "You are a SANDWICH!"
+  winning_paragraph = "You are reliable and trustworthy. Your friends couldn't get by without you!"
+elif winner == "b":
+  winning_result = "You are an ICE CREAM SUNDAE!"
+  winning_paragraph = "You are super sweet and loveable. People turn to you when they need some cheering up."
+elif winner == "c":
+  winning_result = "You are SUSHI!"
+  winning_paragraph = "You are edgy and trendy. Others look to you for a fun and unique adventure."
+elif winner == "d":
+  winning_result = "You are PIZZA!"
+  winning_paragraph = "Your are the life of the party. Everyone want a piece of you!"
+else:
+  print ("Congrats you broke my code somehow...you're a PC...")
+print (winning_result)
+print (winning_paragraph)
