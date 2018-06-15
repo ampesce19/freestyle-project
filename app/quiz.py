ce5ab89f61963abf8a9fbe7b0fa1907b466706f5
ampesce19 <39268723+ampesce19@users.noreply.github.com>
2018-06-15 11:51:02 -0400

    Create quiz.py
    create file and quiz questions

---
+++ app/quiz.py
@@ -0,0 +1,18 @@
+#ampesce19
+#freestyle_project
+#quiz 
+
+import random
+
+#a=sandwich (reliable, always there), b=ice cream sundae (sweet, crowd-pleasing), c=hot dog (quirky, not for everyone), d= pizza (big personality, life of the party)  
+
+questions=["What is your dream job?","Who is your ideal BFF?","What animal are you most like?","What city do you want to move to?","What is your ideal Friday night?","What is your favorite thing to drink?","How often do you try new foods?","Where do you like to shop?"]
+a=["Accountant","Jason Bateman","Cat","Cleveland","Movie night with friends","Water","Never","Department store"]
+b=["Teacher","Jennifer Aniston","Rabbit","Montreal","Dinner at your favorite spot","Wine","Occassionally","Local shops"]
+c=["Artist","Russell Brand","Sloth","Tokyo","Museum event","Cosmopolitan","All the time","Trendy boutiques"]
+d=["Comedian","Kevin Hart","Dog","Barcelona","Partying","Beer","If everyone else is trying it","Mall"]
+
+atotal=0
+btotal=0
+ctotal=0
+dtotal=0
