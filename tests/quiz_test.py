f26aa7cb6d85671ebd71291f52bb99d263cf133b
ampesce19 <39268723+ampesce19@users.noreply.github.com>
2018-06-20 17:44:18 -0400

    Update quiz_test.py

--- tests/quiz_test.py
+++ tests/quiz_test.py
@@ -1,25 +1,30 @@
 import pytest
 from app.quiz import response_count
 
-#if __name__ == "__main__":
-#    response_count ()
+test_1 = response_count()
 
 def test_response_count_1():
-    answers == "a"
+    test_1 == "a"
     assert atotal == 1
     #expected to pass
 
+test_2 = response_count()
+
 def test_response_count_2():
-    answers == "c"
+    test_2 == "c"
     assert btotal == 1
     #expected to fail
 
+test_3 = response_count()
+
 def test_response_count_3():
-    answers == "B"
+    test_3 == "B"
     assert btotal ==1
     #expected to pass - checking capitalized letter
 
+test_4 = response_count()
+
 def test_response_count_4():
-    answers == 1234
-    assert atotal = btotal = ctotal = dtotal == 0
+    test_4 == 1234
+    assert atotal == btotal == ctotal == dtotal == 0
     #expected to pass
